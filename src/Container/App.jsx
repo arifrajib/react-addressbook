768532d1d48dc2c79e235e5b38c11191ef4c7406
Ariful Islam Rajib <arif.rajib@gmail.com>
2016-06-10 21:42:37 -0700

    Page Header added

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-
+import {
+	PageHeader
+} from 'react-bootstrap';
 
 class App extends Component {
 	constructor (){
 		super();
 	}
 	render (){
-		return <p>hi</p>;
+		return (
+			<div>
+				<PageHeader>
+					React AddressBook
+				</PageHeader>
+			</div>
+		);
 	}
 }
 
